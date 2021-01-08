926f5ea1c2df7ddeb38a869c7b3ce6bcf27c892e
Minh Tran <minh.tran@asnet.com.vn>
2021-01-08 17:08:32 +0700

    init sample with iris

---
+++ iris_pd/src/iris_pd/io/base64_data_set.py
@@ -0,0 +1,18 @@
+from kedro.io.core import AbstractDataSet, DataSetError
+
+from base64 import b64encode
+
+
+class Byte64DataSet(AbstractDataSet):
+    def __init__(self, filepath):
+        self._filepath = filepath
+
+    def _save(self, binary_data):
+        with open(str(self._filepath), "w") as f:
+            f.write(b64encode(binary_data).decode("utf8"))
+
+    def _load(self):
+        raise DataSetError("Write Only DataSet")
+
+    def _describe(self):
+        return dict(filepath=self._filepath)
---
+++ iris_pd/src/iris_pd/io/byte_data_set.py
@@ -0,0 +1,16 @@
+from kedro.io.core import AbstractDataSet, DataSetError
+
+
+class ByteDataSet(AbstractDataSet):
+    def __init__(self, filepath):
+        self._filepath = filepath
+
+    def _save(self):
+        raise DataSetError("Read Only DataSet")
+
+    def _load(self):
+        with open(str(self._filepath), "rb") as f:
+            return f.read()
+
+    def _describe(self):
+        return dict(filepath=self._filepath)
